eb5bffeeebbc2917f994b256ef433985b7062d06
mazen sabry bahnes <128668286+mazensabry2712@users.noreply.github.com>
2026-09-13 11:18:24 +0300

    Fix load status after custody return to vehicle

--- app/Actions/ReturnCustodyAction.php
+++ app/Actions/ReturnCustodyAction.php
@@ -78,6 +78,7 @@ class ReturnCustodyAction
             if ($destination === 'vehicle') {
                 $load->on_vehicle_crates_count += $cratesCount;
                 $load->on_vehicle_weight_kg = round((float) $load->on_vehicle_weight_kg + $netWeightKg, 3);
+                $load->refreshStatus();
                 $load->save();
             } else {
                 $stock = ColdStoreStock::query()
