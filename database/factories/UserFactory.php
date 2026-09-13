23b261e5bac9ca7b1efc9f8baee74beb7c9c528b
mazen sabry bahnes <128668286+mazensabry2712@users.noreply.github.com>
2026-09-13 10:47:16 +0300

    Make UserFactory authorization defaults explicit

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -30,16 +30,28 @@ class UserFactory extends Factory
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
+            'role' => 'worker',
+            'is_active' => true,
         ];
     }
 
     /**
-     * Indicate that the model's email address should be unverified.
+     * Indicate that the user's email address should be unverified.
      */
     public function unverified(): static
     {
         return $this->state(fn (array $attributes) => [
             'email_verified_at' => null,
+        ]);
+    }
+
+    /**
+     * Indicate that the user should be inactive.
+     */
+    public function inactive(): static
+    {
+        return $this->state(fn (array $attributes) => [
+            'is_active' => false,
         ]);
     }
 }
